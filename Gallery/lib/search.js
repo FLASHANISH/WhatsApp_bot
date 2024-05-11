e29916e2df0b12e814e69ae164b4e94e8c1f805d
FLASHANISH <102818388+FLASHANISH@users.noreply.github.com>
2024-05-11 13:02:16 +0545

    Add files via upload

---
+++ Gallery/lib/search.js
@@ -0,0 +1,22 @@
+const prefix = global.prefa
+  const pushname = m.pushName || "No Name"
+
+const searchmenu=`「 *wa bot* 」
+♡ۣۜۜ፝͜͜͡͡✿➤ *INFO BOT*
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Name: wa bot
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Version: 1.0
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Type: VPS
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Owner: Anish
+  ♡ۣۜۜ፝͜͜͡͡✿➤ wa.me/9779811216964
+  ♡ۣۜۜ፝͜͜͡͡✿➤ Prefix: 「  ${prefix}  」
+ ♡ۣۜۜ፝͜͜͡͡✿➤Menu Type: 𝑺𝒆𝒂𝒓𝒄𝒉-𝒎𝒆𝒏𝒖
+ ----------------------------------
+🍁 *Search Commands* 🍁
+⌬${prefix}weather
+⌬${prefix}coffee
+⌬${prefix}google
+
+🌹𝒎𝒐𝒓𝒆 𝒄𝒐𝒎𝒎𝒂𝒏𝒅𝒔 𝒄𝒐𝒎𝒊𝒏𝒈 𝒔𝒐𝒐𝒏
+ `
+ module.exports = searchmenu
+ 
